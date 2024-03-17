7ad311f3ac9b4ef32fb476b310110f5a8aa3e1e5
Jeshan1 <jectionjection@gmail.com>
2024-03-17 16:09:25 +0545

    just for fun

--- script.js
+++ script.js
@@ -24,10 +24,45 @@ function addTask(){
     
         tasks.push(task)
         clearForm()
+        renderData()
     }
 
     console.log(tasks)
 }
+
+function renderData(){
+
+    const tasklist = document.getElementById('tasklist')
+    tasklist.innerHTML = ""
+
+    tasks.forEach((task,index)=>{
+        const listItem = document.createElement('li')
+        
+        listItem.innerHTML = `
+          <strong>${task.title}</strong> - ${task.deadline} - Priority: ${task.priority}
+          <button onclick="toggleCompletion(${index})">Complete</button>
+          <button onclick="deleteTask(${index})">View Details</button>
+          <button onclick="deleteTask(${index})">Delete</button>
+        `
+        if (task.completed) {
+            listItem.classList.add('completed')
+        }
+        tasklist.appendChild(listItem)
+    })
+}
+
+function toggleCompletion(index){
+    tasks[index].completed = !tasks[index].completed
+    renderData()
+}
+
+function deleteTask(index){
+    tasks.splice(index,1)
+    renderData()
+}
+
+//filtering data
+
 
 
 
